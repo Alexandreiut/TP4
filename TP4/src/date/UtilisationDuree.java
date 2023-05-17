21b4f49c192f96fddcc298e60ab2574a3bc27fde
ALEXANDRE BROUZES <alexandre.brouzes@iut.local>
2023-05-17 17:28:27 +0200

    gg

--- TP4/src/date/UtilisationDuree.java
+++ TP4/src/date/UtilisationDuree.java
@@ -3,6 +3,8 @@
  */
 
 package date;
+import date.DureeHMS;
+import java.util.Scanner;
 
 /**
  * Utilisation des méthodes traitant les durées
@@ -13,7 +15,57 @@ public class UtilisationDuree {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         DureeHMS.testChaine();
+         testChaine();
+    }
+    /** TODO test de la methode entre différente heure
+     * 
+     */
+    public static void testDifferente() {
+        int[] d1; // première durée
+        int[] d2; // deuxième durée
+        int[] d3; // troisième
+
+        d1 = DureeHMS.saisirDuree("Saisie de la première durée : ");
+        d2 = DureeHMS.saisirDuree("Saisie de la deuxième durée : ");
+        d3 = DureeHMS.saisirDuree("Saisie de la troisième durée : ");
+
+        System.out.println("Parmi ces 3 durées, "
+                + DureeHMS.combienDifferentes(d1, d2, d3) + " sont différentes.");
+
+    }
+
+    /** TODO test de la méthode qui ordone les date dans l'ordre croissant
+     * 
+     */
+    public static void testOrdre() {
+        int[] d1; // première durée
+        int[] d2; // deuxième durée
+        int[] d3; // troisième
+
+        d1 = DureeHMS.saisirDuree("Saisie de la première durée : ");
+        d2 = DureeHMS.saisirDuree("Saisie de la deuxième durée : ");
+        d3 = DureeHMS.saisirDuree("Saisie de la troisième durée : ");
+
+        System.out.println("ces 3 durées sont "
+                + (DureeHMS.ordre(d1, d2, d3) ? " ordonnées " : " dans le désordre"));
+
+    }
+
+  
+
+    /** TODO methode qui demande une chaine et qui appelle les sous programme nécéssaire
+     * 
+     */
+    public static void testChaine() {
+        Scanner entree = new Scanner(System.in);
+        String phrase;
+        System.out.print("Entrez une chaine : ");
+        phrase = entree.nextLine();
+        if (DureeHMS.estChaineDuree(phrase)) {
+            System.out.println("OK !!");
+        } else {
+            System.out.println("ERREUR !!! ");
+        }
     }
 
 }
